3947d58c7de40b4a41537382372795509f9df968
Alexandre Jacobs <jacalex@live.be>
2018-02-10 15:48:04 +0100

    finish

--- code/punctationsStatistics.py
+++ code/punctationsStatistics.py
@@ -3,27 +3,44 @@ import string # pour avoir liste de ponctuation
 from sklearn.feature_extraction.text import CountVectorizer
 import pandas as pd
 
-class PunctationStatistics():
-    pass
+class PunctationStatisticsVectorizer(CountVectorizer):
+
+    def __init__(self):
+        super(PunctationStatisticsVectorizer, self).__init__()
+
+    def prepare_article(self, article):
+
+        punctuation_list = list(string.punctuation)
+        additional_punctuation = ['``', '--', '\'\'']
+        punctuation_list.extend(additional_punctuation)
+        article = article.replace("\\r\\n"," ")
+        for char in  article:
+            if char not in punctuation_list:
+                article = article.replace(char,"")
+        return article
+
+    def build_analyzer(self):
+        preprocess = self.build_preprocessor()
+        return lambda article : preprocess(self.decode(self.prepare_article(article)))
 
 def extract_articles(data_file):
 
-    dict_articles = {0:[], 1:[]}
+    list_articles = []
     data_file_df = pd.read_csv(data_file, names=["code","text"])
-    print(len(data_file_df.index))
-    for i in range(len(data_file_df.index)):
-        if data_file_df.iloc[i]["code"]=="0":
-            dict_articles[0].append(data_file_df.iloc[i]["text"])
-        else:
-            dict_articles[1].append(data_file_df.iloc[i]["text"])
-    return dict_articles
-    
+    for i in range(1,len(data_file_df.index)):
+        list_articles.append(data_file_df.iloc[i]["text"])
+    return list_articles
+
 def main():
-    parser = argparse.ArgumentParser(description='Punctation Statistics from a list of articles')
+    parser = argparse.ArgumentParser(description='Punctation Statistics from csv file')
     parser.add_argument('-t', "--trainset", action='store', default=None, help=('Path to csv file '"[default: %(default)s]"))
     args = parser.parse_args()
     working_file = args.trainset
-    dict_articles = extract_articles(working_file)
+    list_articles = extract_articles(working_file)
+    punctuation_statistics_vect = PunctationStatisticsVectorizer()
+    punctuation_statistics_matrix = punctuation_statistics_vect.fit_transform(list_articles)
+    # print(punctuation_statistics_matrix)
+    # print(punctuation_statistics_vect.vocabulary_)
 
 if __name__ == '__main__':
     main()

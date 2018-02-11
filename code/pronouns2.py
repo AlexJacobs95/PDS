fa96652c0dba7cc90b180d4e8d701e22dce580f0
David <David.Engelman@ulb.ac.be>
2018-02-11 15:38:19 +0100

    je compte mnt la fréquence relative

--- code/pronouns2.py
+++ code/pronouns2.py
@@ -1,23 +1,38 @@
 import pandas as pd
 import numpy as np
 import spacy
+import time
+import string
+
 
 class PronounExtractor:
     """ Does a part-of-speech tagging to extract all pronouns
-         Returns a n x 7 (for the 7 pronouns) matrix of values (n rows, 7 columns)  """
+    Returns a n x 7 (for the 7 pronouns) matrix of values (n rows, 7 columns)
+    each line contains the relative frequency of each pronouns
+          """
 
     def __init__(self):
         self.nlp = spacy.load('en')
         self.pronoun_list = ["i", "you", "she", "he", "it", "we", "they"]
 
+    def remove_punctuation(self, text):
+        translator = str.maketrans('', '', string.punctuation)
+        return text.translate(translator)
+
     def extract(self, article):
 
         res = [0 for _ in range(len(self.pronoun_list))]
+        article = self.remove_punctuation(article)
+        size = len(article)
         doc = self.nlp(article)
+
         for token in doc:
-                if token.tag_ == "PRP":
-                    if token.text.lower() in self.pronoun_list:
-                        res[self.pronoun_list.index(token.text.lower())] += 1
+            if token.tag_ == "PRP":
+                if token.text.lower() in self.pronoun_list:
+                    res[self.pronoun_list.index(token.text.lower())] += 1
+
+        res = [val / size for val in res]
+
         return res
 
     def transform(self, data):
@@ -26,12 +41,16 @@ class PronounExtractor:
         for article in data:
             print(i)
             results.append(self.extract(article))
-            i+=1
+            i += 1
         return np.vstack(results)
 
 
 if __name__ == '__main__':
-    dataframe_test = pd.read_csv("../dataset/train_bis.csv")
+    dataframe_test = pd.read_csv("../dataset/balancedtest_bis.csv")
     extractor = PronounExtractor()
+    start = time.time()
     results = extractor.transform(dataframe_test.text)
-    print(results)
+    extract_time = time.time() - start
+    print("extract time: %0.3fs" % extract_time)
+    df = pd.DataFrame(results, columns=["i", "you", "she", "he", "it", "we", "they"])
+    df.to_csv("pronouns_feature.csv")

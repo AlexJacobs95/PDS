bf8d41b97677242efcc093a0ea44801b40bcebe0
David <David.Engelman@ulb.ac.be>
2018-02-12 14:17:59 +0100

    refactor

--- code/pronouns2.py
+++ code/pronouns2.py
@@ -39,7 +39,6 @@ class PronounExtractor:
 
         return res
 
-
     def transform(self, data):
         personal_results = []
         possessive_results = []
@@ -48,24 +47,25 @@ class PronounExtractor:
 
         i = 1
         for article in data:
-            #print(i)
-            personal_results.append(self.extract(article,self.personal_pronoun_list, "PRP"))
-            possessive_results.append(self.extract(article,self.possessive_pronoun_list, "PRP$"))
-            wh_personal_results.append(self.extract(article,self.wh_personal_pronoun_list, "WP"))
-            wh_possessive_results.append(self.extract(article,self.wh_possessive_pronoun_list, "WP$"))
+            # print(i)
+            personal_results.append(self.extract(article, self.personal_pronoun_list, "PRP"))
+            possessive_results.append(self.extract(article, self.possessive_pronoun_list, "PRP$"))
+            wh_personal_results.append(self.extract(article, self.wh_personal_pronoun_list, "WP"))
+            wh_possessive_results.append(self.extract(article, self.wh_possessive_pronoun_list, "WP$"))
             i += 1
-        return np.vstack(personal_results) , np.vstack(possessive_results), np.vstack(wh_personal_results), np.vstack(wh_possessive_results)    
-  
+        return np.vstack(personal_results), np.vstack(possessive_results), np.vstack(wh_personal_results), np.vstack(
+            wh_possessive_results)
+
+
 if __name__ == '__main__':
-    
     dataframe_test = pd.read_csv("../dataset/balancedtest_bis.csv")
     extractor = PronounExtractor()
     start = time.time()
-    
+
     r1, r2, r3, r4 = extractor.transform(dataframe_test.text)
     extract_time = time.time() - start
     print("extract time: %0.3fs" % extract_time)
-    
+
     df = pd.DataFrame(r1, columns=["i", "you", "she", "he", "it", "we", "they"])
     df.to_csv("personal_pronouns_feature.csv")
 
@@ -76,6 +76,4 @@ if __name__ == '__main__':
     df.to_csv("wh_personal_pronouns_feature.csv")
 
     df = pd.DataFrame(r4, columns=["whose", "whosever"])
-    df.to_csv("wh_possessive_pronouns_feature.csv") 
-
-    
+    df.to_csv("wh_possessive_pronouns_feature.csv")

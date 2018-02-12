eee1410b05f2235554a64482ea4d7f9e4dbdb4fc
harokb <harokb@gmail.com>
2018-02-12 18:59:27 +0100

    Added requirements file (list of Python packages of project) and clean up

--- code/TextCountExtractor.py
+++ code/TextCountExtractor.py
@@ -6,20 +6,18 @@ class TextCountExtractor:
     """
     Extracts
      - the number of words in each article
-     - the number of characters of the article
+     - the number of characters in each article
     If there are n articles, it returns a n x 2 matrix of values (n rows, 2 columns)
     """
-
-    def getInfo(self, article):
+    def getArticleInfo(self, article):
         length = len(article)
         wordCount = len(article.split())
         return [length, wordCount]
 
     def transform(self, data):
-        results = []
-        for article in data:
-            results.append(self.getInfo(article))
+        results = [self.getArticleInfo(article) for article in data]
         return np.vstack(results)
+
 
 
 if __name__ == '__main__':

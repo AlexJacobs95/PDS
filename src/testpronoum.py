dd94a7c7fafb28d981afd2b9d717aaab6245f0b2
David <David.Engelman@ulb.ac.be>
2018-02-13 18:27:21 +0100

    testpronouns update

--- src/testpronoum.py
+++ src/testpronoum.py
@@ -2,7 +2,52 @@ from sklearn.feature_extraction.text import CountVectorizer
 import pandas as pd
 import time
 import spacy
-from tqdm import tqdm
+import string
+import argparse
+
+punctuations = string.punctuation
+nlp = spacy.load('en', disable=['parser', 'ner'])
+
+
+punctuations_list = [elem for elem in string.punctuation]
+prononus = ["i", "you", "she", "he", "it", "we", "they"] + ["mine", "yours", "his", "hers", "ours", "theirs"] + ["what", "who", "whom"] + ["whose", "whosever"]
+
+
+
+
+def spacy_tokenizer(sentence):
+    tokens = nlp(sentence)
+    tokens = [tok.text for tok in tokens if (tok.text in punctuations or tok.tag_ in ["PRP", "PRP$", "WP", "WP$"])]
+    return tokens
+
+
+class Extractor:
+    def __init__(self):
+        self.vectorizer = CountVectorizer(vocabulary = prononus + punctuations_list, tokenizer=spacy_tokenizer)
+
+
+    def extract_train(self, data):
+        print("Extracting...")
+        t0 = time.time()
+        features = self.vectorizer.fit_transform(data.text)
+        extract_time = time.time() - t0
+        print("extract time: %0.3fs" % extract_time)
+
+        return features
+
+    def extract_test(self, data):
+        print("Extracting..")
+        t0 = time.time()
+        features = self.vectorizer.transform(data.text)
+        extract_time = time.time() - t0
+        print("extract time: %0.3fs" % extract_time)
+
+        return features
+
+    def get_vectorizer(self):
+        return self.vectorizer
+
+
 
 def csrtomatrix(data, vocabulary):
     matrix = [[0 for _ in range(len(vocabulary))] for _ in range(data.shape[0])]
@@ -20,15 +65,23 @@ def create_csv_file(data, vocabulary, output_file):
     table.index = [str(i) for i in range(len(data))]
     table.to_csv(output_file)
 
-if __name__ == '__main__':
-    all_data = pd.read_csv("../dataset/balancedtest.csv")
-    res_tokens = []
-    parser = spacy.load("en", disable=['parser', 'ner'])
-    for data in tqdm(all_data.text):
-        tokens = parser(data)
-        token_pronouns = [token.text for token in tokens if token.tag_ in ["PRP", "PRP$", "WP", "WP$"]]
-        res_tokens.append(" ".join(token_pronouns))
-    count_pronoum_vect = CountVectorizer()
-    pronoun_csr_matrix = count_pronoum_vect.fit_transform(res_tokens)
 
-    # create_csv_file(pronoun_csr_matrix, count_pronoum_vect.vocabulary_, '../dataset/features/result_extraction_pronoum.csv')
+def main():
+    parser = argparse.ArgumentParser(description='Punctation Statistics from csv file')
+    parser.add_argument('-t', "--trainset", action='store',
+                        default="../dataset/test_OK.csv",
+                        help='Path to csv file '"[default: %(default)s]")
+    parser.add_argument('-o', "--output", action='store',
+                        default='../dataset/features/result_extraction.csv',
+                        help='Path to csv file '"[default: %(default)s]")
+    args = parser.parse_args()
+    working_file = args.trainset
+    output_file = args.output
+    data = pd.read_csv(working_file)
+    extractor = Extractor()
+    features = extractor.extract_train(data)
+    print(extractor.vectorizer.vocabulary_)
+    create_csv_file(features, extractor.vectorizer.vocabulary_, output_file)
+
+if __name__ == '__main__':
+    main()

3a4055e58c0cbe2f8730424ca9c0acfb83f531f9
David <David.Engelman@ulb.ac.be>
2018-02-13 12:37:11 +0100

    update

--- src/punctationsStatistics.py
+++ src/punctationsStatistics.py
@@ -28,8 +28,7 @@ class PunctuationStatisticsVectorizer(CountVectorizer):
         tokens = self.nlp(article)
         tokens_punctuation = [token.orth_ for token in tokens if token.is_punct]
         token_pronouns = [token.text for token in tokens if token.tag_ in ["PRON", "PRP$", "WP", "WP$"]]
-        tokens_needed = "".join(tokens_punctuation + token_pronouns)
-        print(tokens_needed)
+        tokens_needed = " ".join(tokens_punctuation + token_pronouns)
 
         return tokens_needed
 

dd538f36b55625da6510c27e0e8e7b54dd9352f1
Benjamin <Benjamin.Engelman@ulb.ac.be>
2018-02-16 11:31:37 +0100

    Deleted site_web + created site

--- site/app.py
+++ site/app.py
@@ -7,3 +7,17 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/game')
+def game():
+    return render_template('game.html')
+
+
+@app.route('/checkAnswer')
+def checkAnswer():
+    raise NotImplementedError
+
+
+def getAnswer(article_id):
+    raise NotImplementedError

974d802441fa5288f00cd5d7bc0c7fe1ea16cd72
David <David.Engelman@ulb.ac.be>
2018-02-17 19:04:21 +0100

    update features extractor

--- src/extractFeatures.py
+++ src/extractFeatures.py
@@ -13,8 +13,29 @@ TEST = pd.read_csv("../dataset/test_20.csv")
 SENTIMENT_DATA = pd.read_csv("../resources/emotion.csv")
 
 
-def extractFeature(feature_name, dataset):
-    print("Extracting " + feature_name + " from train")
+def extractFeatureWithVectorizer(extractor, feature_name, dataset, train=False):
+    print("Extracting " + feature_name)
+    start = time.time()
+
+    if train:
+        features = extractor.extract_train(dataset)
+
+    else:
+        features = extractor.extract_test(dataset)
+
+    if train:
+        path = '../features/' + feature_name + '_tra!n_features.npz'
+    else:
+        path = '../features/' + feature_name + '_test_features.npz'
+
+    sparse.save_npz(path, features)
+
+    extract_time = time.time() - start
+    print("extract time: %0.3fs" % extract_time)
+
+
+def extractFeature(feature_name, dataset, train=False):
+    print("Extracting " + feature_name)
     start = time.time()
 
     if feature_name == "text_count":
@@ -26,123 +47,126 @@ def extractFeature(feature_name, dataset):
     elif feature_name == "sentiment":
         features = sparse.csr_matrix(SentimentExtractor(sentiment_data).words_classifier(train))
 
-    elif feature_name in ["tfidf", "prounouns", "punctuation"]:
-        if feature_name == "tfidf":
-            extractor = TfidfExtractor(ngram=1)
-
-        elif feature_name == "pronouns":
-            extractor = PronounsExtractor()
-
-        else:
-            extractor = PunctuationExtractor()
-
-        if dataset == TRAIN:
-            features = extractor.extract_train(dataset)
-
-        else:
-            features = extractor.extract_test(dataset)
-
     else:
         print("Wrong feature name.")
         return
 
-    sparse.save_npz('../features/' + feature_name + '_' + dataset.lower() + '_features.npz', features)
+    if train:
+        path = '../features/' + feature_name + '_tra!n_features.npz'
+    else:
+        path = '../features/' + feature_name + '_test_features.npz'
+
+    sparse.save_npz(path, features)
 
     extract_time = time.time() - start
     print("extract time: %0.3fs" % extract_time)
 
 
 if __name__ == '__main__':
-    for feature_name in ['tfidf', 'prounouns', 'punctuation', 'text_count', 'readability', 'sentiment']:
-        extractFeature(feature_name, TRAIN)
+    for feature_name in ['text_count', 'readability', 'sentiment']:
+        extractFeature(feature_name, TRAIN, train=True)
         extractFeature(feature_name, TEST)
 
+    max_features = 500
+    extractor = TfidfExtractor(ngram=1, max_features=max_features)
+    extractFeatureWithVectorizer(extractor, 'tfidf_' + str(max_features), TRAIN, train=True)
+    extractFeatureWithVectorizer(extractor, 'tfidf_' + str(max_features), TEST)
 
-    # train = pd.read_csv("../dataset/train_80.csv")
-    # test = pd.read_csv("../dataset/test_20.csv")
-    # sentiment_data = pd.read_csv("../resources/emotion.csv")
-    #
-    # start_total = time.time()
-    #
-    # print("extracting tfidf from train...")
-    # start = time.time()
-    # extractor = TfidfExtractor(ngram=1)
-    # features = extractor.extract_train(train)
-    # sparse.save_npz('../features/tfidf_train_features.npz', features)
-    # extract_time = time.time() - start
-    # print("extract time: %0.3fs" % extract_time)
-    # print("extracting tfidf from test...")
-    # start = time.time()
-    # features = extractor.extract_test(test)
-    # sparse.save_npz('../features/tfidf_test_features.npz', features)
-    # extract_time = time.time() - start
-    # print("extract time: %0.3fs" % extract_time)
-    #
-    # print("extracting punctuation from train...")
-    # start = time.time()
-    # extractor = PunctuationExtractor()
-    # features = extractor.extract_train(train)
-    # sparse.save_npz('../features/punctuations_train_features.npz', features)
-    # extract_time = time.time() - start
-    # print("extract time: %0.3fs" % extract_time)
-    # print("extracting punctuation from test...")
-    # start = time.time()
-    # features = extractor.extract_test(test)
-    # sparse.save_npz('../features/punctuations_test_features.npz', features)
-    # extract_time = time.time() - start
-    # print("extract time: %0.3fs" % extract_time)
-    #
-    # print("extracting pronouns from train...")
-    # start = time.time()
-    # extractor = PronounsExtractor()
-    # features = extractor.extract_train(train)
-    # sparse.save_npz('../features/pronouns_train_features', features)
-    # extract_time = time.time() - start
-    # print("extract time: %0.3fs" % extract_time)
-    # print("extracting pronouns from test...")
-    # start = time.time()
-    # features = extractor.extract_test(test)
-    # sparse.save_npz('../features/pronouns_test_features', features)
-    # extract_time = time.time() - start
-    # print("extract time: %0.3fs" % extract_time)
-    #
-    # print("extracting text_count from train...")
-    # start = time.time()
-    # features = sparse.csr_matrix(TextCountExtractor().transform(train))
-    # sparse.save_npz('../features/text_count_train_features', features)
-    # extract_time = time.time() - start
-    # print("extract time: %0.3fs" % extract_time)
-    # print("extracting text_count from test...")
-    # start = time.time()
-    # features = sparse.csr_matrix(TextCountExtractor().transform(test))
-    # sparse.save_npz('../features/text_count_test_features', features)
-    # extract_time = time.time() - start
-    # print("extract time: %0.3fs" % extract_time)
-    #
-    # print("extracting readability from train...")
-    # start = time.time()
-    # features = sparse.csr_matrix(readability_score(train))
-    # sparse.save_npz('../features/readablity_train_features.npz', features)
-    # extract_time = time.time() - start
-    # print("extract time: %0.3fs" % extract_time)
-    # print("extracting readability from test...")
-    # features = sparse.csr_matrix(readability_score(test))
-    # sparse.save_npz('../features/readablity_test_features.npz', features)
-    # extract_time = time.time() - start
-    # print("extract time: %0.3fs" % extract_time)
-    #
-    # print("extracting sentiment from train...")
-    # start = time.time()
-    # features = sparse.csr_matrix(SentimentExtractor(sentiment_data).words_classifier(train))
-    # sparse.save_npz('../features/sentiment_train_features.npz', features)
-    # extract_time = time.time() - start
-    # print("extract time: %0.3fs" % extract_time)
-    # print("extracting sentiment from test...")
-    # start = time.time()
-    # features = sparse.csr_matrix(SentimentExtractor(sentiment_data).words_classifier(test))
-    # sparse.save_npz('../features/sentiment_test_features.npz', features)
-    # extract_time = time.time() - start
-    # print("extract time: %0.3fs" % extract_time)
-    #
-    # extract_total = time.time() - start_total
-    # print("total extract time: %0.3fs" % extract_total)
+    extractor = PunctuationExtractor()
+    extractFeatureWithVectorizer(extractor, 'punctuation', TRAIN)
+    extractFeatureWithVectorizer(extractor, 'punctuation', TEST)
+
+    extractor = PronounsExtractor()
+    extractFeatureWithVectorizer(extractor, 'pronouns', TRAIN)
+    extractFeatureWithVectorizer(extractor, 'prounouns', TEST)
+
+
+
+        # train = pd.read_csv("../dataset/train_80.csv")
+        # test = pd.read_csv("../dataset/test_20.csv")
+        # sentiment_data = pd.read_csv("../resources/emotion.csv")
+        #
+        # start_total = time.time()
+        #
+        # print("extracting tfidf from train...")
+        # start = time.time()
+        # extractor = TfidfExtractor(ngram=1)
+        # features = extractor.extract_train(train)
+        # sparse.save_npz('../features/tfidf_train_features.npz', features)
+        # extract_time = time.time() - start
+        # print("extract time: %0.3fs" % extract_time)
+        # print("extracting tfidf from test...")
+        # start = time.time()
+        # features = extractor.extract_test(test)
+        # sparse.save_npz('../features/tfidf_test_features.npz', features)
+        # extract_time = time.time() - start
+        # print("extract time: %0.3fs" % extract_time)
+        #
+        # print("extracting punctuation from train...")
+        # start = time.time()
+        # extractor = PunctuationExtractor()
+        # features = extractor.extract_train(train)
+        # sparse.save_npz('../features/punctuations_train_features.npz', features)
+        # extract_time = time.time() - start
+        # print("extract time: %0.3fs" % extract_time)
+        # print("extracting punctuation from test...")
+        # start = time.time()
+        # features = extractor.extract_test(test)
+        # sparse.save_npz('../features/punctuations_test_features.npz', features)
+        # extract_time = time.time() - start
+        # print("extract time: %0.3fs" % extract_time)
+        #
+        # print("extracting pronouns from train...")
+        # start = time.time()
+        # extractor = PronounsExtractor()
+        # features = extractor.extract_train(train)
+        # sparse.save_npz('../features/pronouns_train_features', features)
+        # extract_time = time.time() - start
+        # print("extract time: %0.3fs" % extract_time)
+        # print("extracting pronouns from test...")
+        # start = time.time()
+        # features = extractor.extract_test(test)
+        # sparse.save_npz('../features/pronouns_test_features', features)
+        # extract_time = time.time() - start
+        # print("extract time: %0.3fs" % extract_time)
+        #
+        # print("extracting text_count from train...")
+        # start = time.time()
+        # features = sparse.csr_matrix(TextCountExtractor().transform(train))
+        # sparse.save_npz('../features/text_count_train_features', features)
+        # extract_time = time.time() - start
+        # print("extract time: %0.3fs" % extract_time)
+        # print("extracting text_count from test...")
+        # start = time.time()
+        # features = sparse.csr_matrix(TextCountExtractor().transform(test))
+        # sparse.save_npz('../features/text_count_test_features', features)
+        # extract_time = time.time() - start
+        # print("extract time: %0.3fs" % extract_time)
+        #
+        # print("extracting readability from train...")
+        # start = time.time()
+        # features = sparse.csr_matrix(readability_score(train))
+        # sparse.save_npz('../features/readablity_train_features.npz', features)
+        # extract_time = time.time() - start
+        # print("extract time: %0.3fs" % extract_time)
+        # print("extracting readability from test...")
+        # features = sparse.csr_matrix(readability_score(test))
+        # sparse.save_npz('../features/readablity_test_features.npz', features)
+        # extract_time = time.time() - start
+        # print("extract time: %0.3fs" % extract_time)
+        #
+        # print("extracting sentiment from train...")
+        # start = time.time()
+        # features = sparse.csr_matrix(SentimentExtractor(sentiment_data).words_classifier(train))
+        # sparse.save_npz('../features/sentiment_train_features.npz', features)
+        # extract_time = time.time() - start
+        # print("extract time: %0.3fs" % extract_time)
+        # print("extracting sentiment from test...")
+        # start = time.time()
+        # features = sparse.csr_matrix(SentimentExtractor(sentiment_data).words_classifier(test))
+        # sparse.save_npz('../features/sentiment_test_features.npz', features)
+        # extract_time = time.time() - start
+        # print("extract time: %0.3fs" % extract_time)
+        #
+        # extract_total = time.time() - start_total
+        # print("total extract time: %0.3fs" % extract_total)

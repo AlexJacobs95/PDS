51f982bc200a7be21046e68ca8d0ebeb0a57977f
harokb <harokb@gmail.com>
2018-02-17 15:05:40 +0100

    Neural network runs (not optimized, architecture copied from Keras.io)

--- src/models/NeuralNetwork.py
+++ src/models/NeuralNetwork.py
@@ -11,12 +11,12 @@ class NeuralNetwork:
         self.model = Sequential()
         self.model.add(Dense(units=100, activation='relu', input_dim=inputSize))
         self.model.add(Dense(units=numLabels, activation='softmax'))
-        self.model.compile(loss='categorial_crossentropy',
+        self.model.compile(loss='binary_crossentropy',
                            optimizer='sgd',
                            metrics=['accuracy'])
 
     def fit(self, articlesTrain, labelsTrain):
-        self.model.fit(articlesTrain, labelsTrain, epochs=5, batch_size=32)
+        self.model.fit(articlesTrain, labelsTrain)
 
     def predict(self, articles):
         return self.model.predict(articles)
@@ -30,6 +30,7 @@ def getFeatureFilePaths():
     currentDir = os.path.dirname(os.path.realpath(__file__))
     featuresDir = os.path.join(currentDir, '..', '..', 'features')
     return [os.path.join(featuresDir, filename) for filename in os.listdir(featuresDir)]
+
 
 def getDatasetDir():
     currentDir = os.path.dirname(os.path.realpath(__file__))
@@ -50,29 +51,31 @@ def loadDataset(useTrainDataset=True):
         - A test set (to test the model): testNews, testLabels
     :param useTrainDataset: if True, use the training dataset, otherwise use the test dataset.
     """
-    features = None
+    features = []
     for file_path in getFeatureFilePaths():
+        # if 'readablity' in file_path:
+        #     continue
+
         if 'test' in file_path and useTrainDataset:
             continue
         elif 'train' in file_path and not useTrainDataset:
             continue
 
         feature = sparse.load_npz(file_path)
+        features.append(feature)
 
-        if features is None:
-            features = feature
-        else:
-            features = sparse.vstack(features, feature)
-
+    # We stack all the features, then convert to CSR to allow slicing row-wise,
+    # which is required to batches in the neural network
+    featureMatrix = sparse.hstack(features).tocsr()
     labels = loadLabels(useTrainDataset)
-    return features, labels
+    return featureMatrix, labels
 
 
 if __name__ == '__main__':
     trainNews, trainLabels = loadDataset(useTrainDataset=True)
     testNews, testLabels = loadDataset(useTrainDataset=False)
-    neuralNetwork = NeuralNetwork(inputSize=trainNews.shape[1], numLabels=trainLabels.shape[1])
-    neuralNetwork.fit(trainNews, trainLabels)
+    neuralNetwork = NeuralNetwork(inputSize=trainNews.shape[1], numLabels=1)
+    neuralNetwork.fit(trainNews, trainLabels.as_matrix())
 
-    loss_and_metrics = neuralNetwork.evaluate(testNews, testLabels)
+    loss_and_metrics = neuralNetwork.evaluate(testNews, testLabels.as_matrix())
     print(loss_and_metrics)

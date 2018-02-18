f691bb62e4ad74f5f2c84cc60385c3b4f2d42de9
David <David.Engelman@ulb.ac.be>
2018-02-18 12:14:07 +0100

    correction

--- site/database/fill_database.py
+++ site/database/fill_database.py
@@ -1,13 +1,28 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-
+import pandas as pd
 import sqlite3 as lite
-con = lite.connect('articles')
-with con:
-    cur = con.cursor()
-    for index in range(1,41):
-        articles = "article{}".format(index)
-        f = open(articles, encoding="utf8")
-        article= f.readlines()
+
+articles = []
+labels = []
+
+for index in range(1,41):
+        label = 1
+        if index <=20:
+            label = 0
+        file = "article{}".format(index)
+        f = open(file, encoding="utf8")
+        article = f.readlines()
         article = " ".join(article)
-        cur.execute("INSERT INTO Articles VALUES({},{},{})".format(int(index), int(2), article))
+        articles.append(article)
+        labels.append(label)
+
+data = pd.DataFrame(data={'label': labels, 'content': articles})
+print(data)
+
+con = lite.connect('articles')
+data.to_sql('articles', con=con, index=True)
+
+
+
+
